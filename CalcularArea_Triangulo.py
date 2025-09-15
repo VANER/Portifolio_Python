164b64cb60d8e857de2d27ef00038455cf3bd0ca
Vâner Menezes <VANERMENEZES@HOTMAIL.COM>
2025-09-15 06:57:52 -0300

    Add files via upload

---
+++ CalcularArea_Triangulo.py
@@ -0,0 +1,7 @@
+# Calcular a área de um triângulo a partir de sua base e altura.
+
+print ('Informe os dados do triângulo') #Escreve na tela para usuario.
+base = float (input ('Base: ')) #Variavel recebendo numero decimal.
+altura = float (input ('Altura: ')) #Variavel recebendo numero decimal.
+area = base * altura / 2 #vaiavel recebendo o calculo da area do triangulo.
+print ('A area do triângulo é ', area, ' m².') #Informa o resultado na tela para usuario.
